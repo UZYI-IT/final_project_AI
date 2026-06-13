d8ad4a7accedcc02c7a37111bf25f1e8cf6b5a93
UZYI-IT <uzyi.it@gmail.com>
2026-06-13 00:01:04 -0400

    init commit

---
+++ emotion_detection.py
@@ -0,0 +1,29 @@
+from flask import Flask, make_response, request
+import requests, json
+
+
+app = Flask(__name__)
+
+def emotion_detector(text_to_analyze):
+
+    # URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Input json: { "raw_document": { "text": text_to_analyze } }
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input = {
+        "raw_document": { "text": text_to_analyze } 
+    }
+    response = requests.post("https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict", headers= headers, json= input )
+    output = response.json()
+    filteredOutput = output['emotionPredictions'][0]['emotion']
+    high_score = max(filteredOutput, key=filteredOutput.get)
+
+    filteredOutput['dominant_emotion']= high_score
+
+
+    print(json.dumps(filteredOutput, indent=2))
+
+    
